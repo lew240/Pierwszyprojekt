9e9658be15f6270a89be4c53e1b7b6a008f8775e
Michal <lew240@wp.pl>
2022-10-16 19:08:35 +0200

    dodanie informacji o komentarzu wielolinijkowym

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
    public class HelloWorld {
          public static void main(String[] args){
+             /*
+            Wywołanie metody System.out.println
+            Wypisuje na ekran tekst "Hello World!"
+        */
+             // to wyżej to komentarz wielolinijkowy
            System.out.println("Hello World"); // Wypisanie tekstu ( komentarz wewnątrz linii )
            // Linie poprzedzone // to komentarze do pracy nad plikiem
        }
